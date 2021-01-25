b31be153b7909b292aba89f0ca4f8a34c14985f6
Ilya <ilyavodop1@gmail.com>
2021-01-25 23:22:51 +0500

    CardSpawn

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class CardView : MonoBehaviour
+{
+    public TextMesh _cardName;
+    public TextMesh _mpCounter;
+    public TextMesh _hpCounter;
+    public TextMesh _damage;
+
+    public SpriteRenderer _cardSprite;
+
+    private int _orderOfLayerOffset;
+
+    public void Init(int order)
+    {
+        _orderOfLayerOffset = order * 10;
+
+
+
+        int mpRan = Random.Range(1, 10);
+        int hpRan = Random.Range(1, 10);
+        int dmgRan = Random.Range(1, 10);
+
+        _mpCounter.text = mpRan.ToString();
+        _hpCounter.text = hpRan.ToString();
+        _damage.text = dmgRan.ToString();
+
+        foreach (var spriteRenderer in GetComponentsInChildren<SpriteRenderer>())
+        {
+           // spriteRenderer.sortingOrder += _orderOfLayerOffset;
+        }
+
+        StartCoroutine(GetTexture());
+    }
+
+
+
+    IEnumerator GetTexture()
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://picsum.photos/190/180");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            _cardSprite.sprite = Sprite.Create((Texture2D)myTexture, new Rect(0.0f, 0.0f, myTexture.width, myTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+    private float posX;
+    private float[] rotatonPos = new float[] { 25, 16.33f, 2.6f, -1.1f, -18.9f, -37.4f };
+    void Start()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            posX = posX + 1f;
+            var spawnedObject = Instantiate(_prefab, new Vector3(posX, _prefab.transform.position.y, _prefab.transform.position.z), Quaternion.identity);
+            spawnedObject.transform.eulerAngles = new Vector3(0, 0, rotatonPos[i]);
+            spawnedObject.GetComponent<CardView>().Init(i);
+        }
+    }
+}
